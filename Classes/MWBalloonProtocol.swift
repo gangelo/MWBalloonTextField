e081351a1c13decfc77e785c57a6cd7cce9afbeb
gangelo <web.gma@gmail.com>
2016-12-26 14:02:06 -0500

    First commit

---
+++ Classes/MWBalloonProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  MWBalloonProtocol.swift
+//  MWBalloonTextField
+//
+//  Created by Gene M. Angelo  Jr. on 12/16/16.
+//  Copyright © 2016 Mohojo Werks LLC. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+protocol MWBalloonProtocol {
+   var balloonTextColor:UIColor { get set }
+   var balloonFontSize:CGFloat { get set }
+   var balloonBackgroundColor:UIColor { get set }
+   var balloonCornerRadius:Int { get set }
+   var balloonBorderWidth:CGFloat { get set }
+   var balloonBorderColor:UIColor { get set }
+   var balloonArrowSize:CGSize { get set }
+   var balloonArrowPosition:CGFloat { get set }
+}
---
+++ MWBalloonTextField/MWBalloonTextField.h
@@ -0,0 +1,17 @@
+//
+//  MWBalloonTextField.h
+//  MWBalloonTextField
+//
+//  Created by Gene M. Angelo  Jr. on 12/23/16.
+//  Copyright © 2016 Mohojo Werks LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWBalloonTextField.
+FOUNDATION_EXPORT double MWBalloonTextFieldVersionNumber;
+
+//! Project version string for MWBalloonTextField.
+FOUNDATION_EXPORT const unsigned char MWBalloonTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWBalloonTextField/PublicHeader.h>
